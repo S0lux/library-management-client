9a2f087b40aea90ce97b959170581b179fb987be
Tkhangds <87261264+Tkhangds@users.noreply.github.com>
2023-12-17 11:58:00 +0700

    Add Member List Layout

--- library-management-client/ViewModels/MemberListViewModel.cs
+++ library-management-client/ViewModels/MemberListViewModel.cs
@@ -1,6 +1,35 @@
+using Avalonia_DependencyInjection.Converters;
+using Avalonia_DependencyInjection.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+
 namespace Avalonia_DependencyInjection.ViewModels;
 
-public class MemberListViewModel:ViewModelBase
+public partial class MemberListViewModel:ViewModelBase
 {
-    
+    public ObservableCollection<MEMBER> memberList { get; set; }
+
+    public MemberListViewModel() 
+    {
+        memberList = new ObservableCollection<MEMBER>();
+    }
+
+    [RelayCommand]
+    public void Add()
+    {
+        memberList.Add(new MEMBER
+        {
+            MemberId = 1,
+            CitizenID = "1",
+            Credit = 1,
+            Name = "Trng Nguyn Trung Khang",
+            Address = "A",
+            PhoneNum = "1",
+            DateOfBirth = DateTime.Now,
+            Gender = 0,
+            EmployeeId = 1
+        });
+    }
 }
