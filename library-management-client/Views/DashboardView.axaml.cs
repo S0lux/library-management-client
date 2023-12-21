6f3769ffce11f49d961f1d505545e335901a0315
S0lux <loctranphuoc456@protonmail.com>
2023-12-21 15:41:11 +0700

    Experimental animation

--- library-management-client/Views/DashboardView.axaml.cs
+++ library-management-client/Views/DashboardView.axaml.cs
@@ -1,6 +1,10 @@
+using System;
 using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Threading;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Avalonia_DependencyInjection.Views;
@@ -16,5 +20,39 @@ public partial class DashboardView : UserControl
     {
         var _mainWindow = App.AppHost!.Services.GetRequiredService<MainWindow>();
         _mainWindow.BeginMoveDrag(e);
+    }
+
+    private void AvaloniaObject_OnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.Property == ContentProperty)
+        {
+            var contentControl = (ContentControl) sender!;
+            var framerate = TimeSpan.FromSeconds(1 / 60.0);
+            var animationSeconds = TimeSpan.FromSeconds(0.25);
+            var totalTicks = animationSeconds.TotalSeconds / framerate.TotalSeconds;
+            var easing = new QuadraticEaseIn();
+            var currentTick = 0;
+            var timer = new DispatcherTimer();
+            
+            timer.Interval = framerate;
+            timer.Tick += (o, args) =>
+            {
+                currentTick++;
+                
+                if (currentTick >= totalTicks)
+                {
+                    timer.Stop();
+                    return;
+                }
+
+                var animationProgress = (double)currentTick / totalTicks;
+                contentControl.Margin = new Thickness(0, 25.0 - (25.0 * easing.Ease(animationProgress)), 0, 0);
+                contentControl.Opacity = animationProgress;
+            };
+
+            contentControl.Margin = new Thickness(0, 25, 0, 0);
+            contentControl.Opacity = 0;
+            timer.Start();
+        }
     }
 }
