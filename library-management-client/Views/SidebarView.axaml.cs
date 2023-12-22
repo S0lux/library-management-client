7a8419372df1b99a953136499471930ad8718977
S0lux <loctranphuoc456@protonmail.com>
2023-12-22 15:41:49 +0700

    WIP animation

--- library-management-client/Views/SidebarView.axaml.cs
+++ library-management-client/Views/SidebarView.axaml.cs
@@ -2,9 +2,12 @@ using System;
 using System.IO;
 using Avalonia;
 using Avalonia_DependencyInjection.ViewModels;
+using Avalonia.Animation.Easings;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
@@ -18,10 +21,38 @@ public partial class SidebarView : UserControl
         InitializeComponent();
     }
 
-    private void Control_OnLoaded(object? sender, RoutedEventArgs e)
+    /*private void Control_OnLoaded(object? sender, RoutedEventArgs e)
     {
-        Console.WriteLine("Hello");
-    }
+        var targetControl = (Grid)sender!;
+        var framerate = TimeSpan.FromSeconds(1 / 90.0);
+        var animationSecs = TimeSpan.FromSeconds(0.25);
+        var easing = new CubicEaseIn();
+        var totalTicks = animationSecs.TotalSeconds / framerate.TotalSeconds;
+        var currentTick = 0;
+
+        var translateTransform = new TranslateTransform(0, 0);
+        targetControl.RenderTransform = translateTransform;
+
+        var timer = new DispatcherTimer();
+        timer.Interval = framerate;
+        timer.Tick += (o, args) =>
+        {
+            currentTick++;
+
+            if (currentTick > totalTicks)
+            {
+                timer.Stop();
+                translateTransform.X = 0;
+                return;
+            }
+
+            var currentProgress = currentTick / totalTicks;
+            translateTransform.X = -240 + 240 * easing.Ease(currentProgress);
+        };
+
+        translateTransform.X = -240;
+        timer.Start();
+    }*/
 
     private async void Button_OnClick(object? sender, RoutedEventArgs e)
     {
@@ -34,7 +65,7 @@ public partial class SidebarView : UserControl
         if (result == ButtonResult.Yes)
         {
             File.Delete("userToken.txt");
-            var win=App.AppHost.Services.GetRequiredService<MainWindowViewModel>();
+            var win=App.AppHost!.Services.GetRequiredService<MainWindowViewModel>();
             win.ContentViewModel = App.AppHost.Services.GetRequiredService<LoginViewModel>();
         }
     }
