6a1bf00545d5ad877ed0120a58b60c2a077caaaf
Tkhangds <87261264+Tkhangds@users.noreply.github.com>
2023-12-22 15:36:02 +0700

    Basic Delete Function

--- library-management-client/ViewModels/MemberListViewModel.cs
+++ library-management-client/ViewModels/MemberListViewModel.cs
@@ -122,7 +122,7 @@ public partial class MemberListViewModel : ViewModelBase
             var body = await response.Content.ReadAsStringAsync();
             var apiResponseMember = JsonConvert.DeserializeObject<ApiResponseMember>(body);
             
-            MemberList = apiResponseMember!.data;
+            MemberList = new ObservableCollection<MEMBER>(apiResponseMember!.data.Where(e => e.Deleted == false));
             ShowingList = MemberList;
         }
         catch (HttpRequestException e)
@@ -158,7 +158,25 @@ public partial class MemberListViewModel : ViewModelBase
 
         var result = await box.ShowAsync();
 
-        
+        if(result == ButtonResult.Yes)
+        {
+            var updateMember = new
+            {
+                MemberID = selectedMember.MemberID,
+                Deleted = true
+            };
+
+            var payload = new
+            {
+                data = updateMember
+            };
+
+            var json = JsonConvert.SerializeObject(payload);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _authService.PutAsync("/api/members", content);
+            GetData();
+        }  
     }
 
     [RelayCommand]
