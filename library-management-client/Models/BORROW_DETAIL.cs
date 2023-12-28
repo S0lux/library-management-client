33c18793ac3954e8a2c746e72939041fb8b7b884
NTDKhoa04 <146697000+NTDKhoa04@users.noreply.github.com>
2023-12-28 13:09:07 +0700

    Bug fixes

--- library-management-client/Models/BORROW_DETAIL.cs
+++ library-management-client/Models/BORROW_DETAIL.cs
@@ -55,9 +55,9 @@ namespace Avalonia_DependencyInjection.Models
 
                 BOOK temp = box.BookList.FirstOrDefault(e => e.ISBN13 == ISBN13);
 
-                if (value > temp.BOOK_DETAILs.First(e => e.Status == "normal").Quantity && temp != null)
+                if ( temp!=null && value > temp.BOOK_DETAILs.First(e => e.Status == "normal").Quantity)
                 {
-                    throw new ArgumentException("Quantity can't be greater than the available amount.",);
+                    throw new ArgumentException("Quantity can't be greater than the available amount.");
                 }
 
                 if (value == 0)
