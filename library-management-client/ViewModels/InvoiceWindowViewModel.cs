54c7c7f83b3b4fa60e8f195831474a2dbc3112cc
S0lux <loctranphuoc456@protonmail.com>
2023-12-28 11:41:02 +0700

    Fixed AddByTitle unable to add book due to _isChecked

--- library-management-client/ViewModels/InvoiceWindowViewModel.cs
+++ library-management-client/ViewModels/InvoiceWindowViewModel.cs
@@ -1,22 +1,26 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using Avalonia_DependencyInjection.Controls;
 using Avalonia_DependencyInjection.Models;
 using Avalonia_DependencyInjection.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Newtonsoft.Json;
 
 namespace Avalonia_DependencyInjection.ViewModels;
 
 public partial class InvoiceWindowViewModel: ViewModelBase
 {
-    [ObservableProperty]
-    private CustomInvoice _invoice;
-
+    [ObservableProperty] private CustomInvoice _invoice;
     [ObservableProperty] private ObservableCollection<BORROW_DETAIL> _borrowDetails = new ObservableCollection<BORROW_DETAIL>();
+    [ObservableProperty] private bool _isLoading;
     
     private readonly AuthenticationService _authService;
 
@@ -27,19 +31,60 @@ public partial class InvoiceWindowViewModel: ViewModelBase
 
     partial void OnInvoiceChanged(CustomInvoice value)
     {
-        foreach (var book in Invoice.BorrowDetails)
-        {
-            Task.Run(async () =>
-            {
-                book.BookTitle = await FetchBookTitle(book.ISBN13);
-                BorrowDetails = new ObservableCollection<BORROW_DETAIL>(value.BorrowDetails);
-            });
-        }
+        PopulateDataGrid();
     }
 
-    partial void OnBorrowDetailsChanged(ObservableCollection<BORROW_DETAIL> value)
+    private async Task PopulateDataGrid()
     {
-        Console.WriteLine("Changed!");
+        IsLoading = true;
+        List<Task> fetchTasks = new List<Task>();
+        
+        foreach (var book in Invoice.BorrowDetails)
+        {
+            var fetchTask = Task.Run(async () =>
+            {
+                book.BookTitle = await FetchBookTitle(book.ISBN13);
+                BorrowDetails = new ObservableCollection<BORROW_DETAIL>(Invoice.BorrowDetails);
+            });
+            
+            fetchTasks.Add(fetchTask);
+        }
+
+        await Task.WhenAll(fetchTasks);
+        IsLoading = false;
+    }
+
+    [RelayCommand]
+    private async Task SaveBookDetails()
+    {
+        IsLoading = true;
+        List<Task> tasks = new List<Task>();
+        
+        // Update borrow details
+        foreach (var borrowDetail in Invoice.BorrowDetails)
+        {
+            var payloadObj = new
+            {
+                InvoiceID = borrowDetail.BorrowInvoiceID,
+                ISBN13 = borrowDetail.ISBN13,
+                Quantity = borrowDetail.Quantity,
+                Returned = borrowDetail.Returned,
+                Damaged = borrowDetail.Damaged,
+                Lost = borrowDetail.Lost,
+            };
+
+            var payloadJson = JsonConvert.SerializeObject(payloadObj);
+            var payloadContent = new StringContent(payloadJson, Encoding.UTF8, "application/json");
+            var task = Task.Run(async () =>
+            {
+                await _authService.PutAsync("/api/borrow_details", payloadContent);
+            });
+            
+            tasks.Add(task);
+        }
+
+        await Task.WhenAll(tasks);
+        IsLoading = false;
     }
 
     private async Task<string> FetchBookTitle(string isbn)
